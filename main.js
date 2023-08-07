611d6a105c520f5ed5153c5b959d74b4f8425789
ekobrk <ekobrk.github@gmail.com>
2023-08-07 22:31:58 +0300

    proje

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
-const url = 'https://api.openweathermap.org/data/2.5/'
-const key = '362f04914f3ecdba012f8d5e7884fd68'
+const url = 'https://api.openweathermap.org/data/2.5/';
+const key = '362f04914f3ecdba012f8d5e7884fd68';
 
 const setQuery = (e) => {
+  if (e.keyCode === 13) {
+    getResult(searchBar.value);
+  }
+};
 
-    if (e.keyCode == '13')
-        getResult(searchBar.value)
-}
+const getResult = (cityName) => {
+  let query = `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=tr`;
+  fetch(query)
+    .then((weather) => weather.json())
+    .then(displayResult);
+};
 
-const getResult=(cityName)==>{
+const displayResult = (result) => {
+  let city = document.querySelector('.city');
+  city.innerText = `${result.name}, ${result.sys.country}`;
 
-    let query=`${url}weather?q${cityName}'&appid=${key}&units=metric&lang=tr`
-    fetch(query)
-    .then(weather==>{
-        return weather.json()
-    })
-    .then(displayResult)
+  let temp = document.querySelector('.temp');
+  temp.innerText = `${Math.round(result.main.temp)}°C`;
 
-    }
+  let desc = document.querySelector('.desc');
+  desc.innerText = result.weather[0].description;
 
-    const displayResult=(result)=>{
+  let minmax = document.querySelector('.minmax');
+  minmax.innerText = `${Math.round(result.main.temp_min)}°C / ${Math.round(result.main.temp_max)}°C`;
+};
 
-        console.log(result);
-    }
-
-        
-
-
-
-const searchBar = document.getElementById('searchBar')
-searchBar.addEventListener('keypress', setQuery)
+const searchBar = document.getElementById('searchBar');
+searchBar.addEventListener('keypress', setQuery);
